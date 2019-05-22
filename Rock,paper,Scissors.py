6d6bd6d2135896b3d0c334e597ec2ca881fef0d3
Felka98 <50754485+Felka98@users.noreply.github.com>
2019-05-22 10:27:19 -0600

    Update Rock,paper,Scissors.py

--- Rock,paper,Scissors.py
+++ Rock,paper,Scissors.py
@@ -1,41 +1,43 @@
 from random import choice
 print("Bienvenido!")
 lista_opciones=["piedra","papel","tijera"]
-computer_input=choice(lista_opciones)
 game_on=True
+def empate():
+    print("Ambos sacaron igual, intentalo de nuevo")
+def win(computer_input):
+    print("La computadora eligio",computer_input,"ganaste")
+def lose(computer_input):
+    print("La computadora eligio",computer_input,"perdiste")
 while game_on:
+    computer_input = choice(lista_opciones)
     print("Elige 0 para piedra,1 para papel,2 para tijera y 3 para terminar partida: ")
     user_input = int(input())
-    if user_input==0:
-        print("\n" * 50)
-        print("Elegiste piedra")
+    if user_input==0: #USUARIO ELIGE PIEDRA
         if computer_input=="piedra":
-            print("Ambos sacaron piedra, intentalo de nuevo")
+            empate()
         elif computer_input=="papel":
-            print("La computadora eligio papel, perdiste.")
+            lose("papel")
         else:
-            print("La computadora eligio tijera, ganaste")
-    elif user_input==1:
-        print("Elegiste papel")
-        print("\n" * 50)
+            win("tijera")
+    elif user_input==1: #USUARIO ELIGE PAPEL
         if computer_input=="papel":
-            print("Ambos sacaron papel, intentalo de nuevo")
+            empate()
         elif computer_input=="piedra":
-            print("La computadora eligio piedra, ganaste")
+            win("piedra")
         else:
-            print("La computadora eligio tijera, perdiste")
-    elif user_input==2:
-        print("Elegiste tijera")
-        print("\n" * 50)
+            lose("tijeras")
+    elif user_input==2: #USUARIO ELIGE TIJERA
         if computer_input=="tijera":
-            print("Ambos sacaron tijera, intentalo de nuevo")
+            empate()
         elif computer_input=="piedra":
-            print("La computadora eligio piedra, perdiste")
+            lose("piedra")
         else:
-            print("La computadora eligio papel,ganaste")
+            win("papel")
     elif user_input==3:
         print("Gracias por jugar!")
         game_on=False
     else:
         print("Invalid input")
+
+
 
